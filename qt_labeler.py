fb535e9f53c702a4a4285ef26a04f0c30d170950
narek <narek@cs.stanford.edu>
2016-10-13 23:42:29 -0700

    Function implemented!

--- qt_labeler.py
+++ qt_labeler.py
@@ -7,10 +7,22 @@ Eval: on N (unseen) questions.
 
 Output: exactly 10 topic (integer id) suggestions (ordered by relevance) for each question in the eval dataset.
 """
-def score(question):
+
+def get_topics(q):
 	pass
-def raw_score(Questions):
-	return sum(score(question) for question in Questions)
+
+def score(q, G):
+	topics = get_topics(q)
+	numer = sum(math.sqrt(10-i) if G[i] in topics else 0 for i in range(10))
+	numTopics = len(topics)
+	denom = sum(math.sqrt(10-i) for i in range(min(numTopics, 10)))
+	ratio = numer/denom
+	return ratio
+
+def raw_score(Q):
+	return sum(score(q) for q in Q)
+
+
 def main():
 	return raw_score([])
 if __name__ == '__main__':
