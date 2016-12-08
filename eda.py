98e213a94b8e896d6e2579b1411c8b1d66e400b5
narek <narek@cs.stanford.edu>
2016-12-07 23:51:02 -0800

    sample plotting of the 2 types of losses

--- eda.py
+++ eda.py
@@ -20,7 +20,7 @@ print(en2lv)
 
 #df = pd.DataFrame.from_dict(en2lv, 'index')
 #df.plot()
-plt.plot([0,1,2,3], en2lv.values())
+plt.plot(range(len(en2lv.keys())), en2lv.values())
 plt.show()
 
 
@@ -36,6 +36,8 @@ twinLosses = glob.glob("*continued*hdf5")
 
 trLosses = [float(fname[fname.find('-')+1:fname.rfind('-')]) for fname in sorted(twinLosses)]
 valLosses = [float(fname[fname.rfind('-')+1:fname.rfind('.')]) for fname in sorted(twinLosses)]
-
+plt.plot([0.2062, 0.2013, 0.2007, 0.1956, 0.1937, 0.1895, 0.1855, 0.1852, 0.1827, 0.1788])
+plt.plot([0.2228, 0.2115, 0.2092, 0.2045, 0.2017, 0.2001, 0.2031, 0.1992, 0.1955, 0.1959])
+plt.show()
 
 
